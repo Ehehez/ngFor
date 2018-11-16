593db3a759f6b6546dbc0cdaf14b5d609421bfce
unknown <DAM220@ACARBALLEIRA.LOCAL>
2018-11-16 13:45:05 +0100

    Ejercicio ngFor

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,13 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  title = 'Tour of Heroes';
+  heroes: string[] = ["Sanic Fax", "Leffen", "Go1", "Kazunoko"];
+  myHero = this.heroes[1];
   constructor(public navCtrl: NavController) {
 
   }
-
+  cambiarHeroe(a: number){
+    this.myHero = this.heroes[a];
+  }
 }
